19dbf3c054ae07f2a2dbfcd8ffeb924fb4760794
Raiziaque <barzizouiyahya@gmail.com>
2019-02-18 20:45:03 +0100

    Quizz=>Quiz

--- src/Routes/RoutesQuiz.ts
+++ src/Routes/RoutesQuiz.ts
@@ -1,19 +1,19 @@
 import express from "express";
 import {QuizController} from '../controllers/QuizController';
 import {UserController} from '../controllers/UserController';
-export class RoutesQuizz {
+export class RoutesQuiz {
     public quizController:QuizController= new QuizController();
     public userController:UserController= new UserController();
     public routes(app:any){
                /*==================== ROUTES FOR QUIZ ========================*/
                 //manipulate quizz by id
-                app.route('/quiz/:quizzId')
+                app.route('/quiz/:quizId')
                 .get(this.quizController.getQuizWithID) 
                 .put(this.quizController.updateQuiz)
                 .delete(this.quizController.deleteQuiz);
                 //add delete user
-                app.route('/quizz') 
-                .get(this.quizController.getQuizzs)        
-                .post(this.quizController.addNewQuizz);        
+                app.route('/quiz') 
+                .get(this.quizController.getQuizs)        
+                .post(this.quizController.addNewQuiz);        
     }
 }
